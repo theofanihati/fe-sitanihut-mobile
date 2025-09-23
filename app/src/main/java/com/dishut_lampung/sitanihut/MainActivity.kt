2b8882d17fed22bc89537f8525e9377a9f12bd33
theofanihati <theofanihati@gmail.com>
2025-09-23 19:41:42 +0700

    initial commit

---
+++ app/src/main/java/com/dishut_lampung/sitanihut/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.dishut_lampung.sitanihut
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.dishut_lampung.sitanihut.presentation.ui.theme.SitanihutTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SitanihutTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    SitanihutTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/dishut_lampung/sitanihut/Sitanihut.kt
@@ -0,0 +1,7 @@
+package com.dishut_lampung.sitanihut
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class Sitanihut : Application()
