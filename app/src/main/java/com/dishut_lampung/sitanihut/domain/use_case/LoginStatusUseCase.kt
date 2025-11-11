1bfcf1d723b0f2aab9ed4914a1dadd43d717a638
theofanihati <theofanihati@gmail.com>
2025-11-11 23:43:47 +0700

    auth domain login status GREEN, ubah use case nya

--- app/src/main/java/com/dishut_lampung/sitanihut/domain/use_case/LoginStatusUseCase.kt
+++ app/src/main/java/com/dishut_lampung/sitanihut/domain/use_case/LoginStatusUseCase.kt
@@ -1,13 +1,11 @@
 package com.dishut_lampung.sitanihut.domain.use_case
 
-import com.dishut_lampung.sitanihut.domain.model.AuthResult
 import com.dishut_lampung.sitanihut.domain.repository.AuthRepository
 
 class LoginStatusUseCase (
     private val authRepository: AuthRepository
     ) {
-        suspend operator fun invoke(): AuthResult<Unit> {
-            // belum diimplementasi, biar test gagal dulu (fase RED)
-            return AuthResult.Error<Unit>("Belum diimplementasi")
+        suspend operator fun invoke(): Boolean  {
+            return authRepository.isLoggedIn()
         }
     }
