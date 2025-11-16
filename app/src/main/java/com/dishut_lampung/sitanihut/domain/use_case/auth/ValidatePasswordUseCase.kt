2ee8ece2825c4f810e73969d705429ce3b3aac93
theofanihati <theofanihati@gmail.com>
2025-11-16 17:48:15 +0700

    change regex pattern style to match kotlin convention

--- app/src/main/java/com/dishut_lampung/sitanihut/domain/use_case/auth/ValidatePasswordUseCase.kt
+++ app/src/main/java/com/dishut_lampung/sitanihut/domain/use_case/auth/ValidatePasswordUseCase.kt
@@ -3,8 +3,49 @@ package com.dishut_lampung.sitanihut.domain.use_case.auth
 import com.dishut_lampung.sitanihut.domain.validator.ValidationResult
 
 class ValidatePasswordUseCase {
+
+    private val kapitalRegex = Regex("[A-Z]")
+    private val lowercaseRegex = Regex("[a-z]")
+    private val numberRegex = Regex("[0-9]")
+    private val specialCharRegex = Regex("[!@#\$%^&*(),.?\":{}|<>]")
+
     operator fun invoke(password: String): ValidationResult {
-        // TODO blumm
-        return ValidationResult(false, errorMessage = "belumm")
+        if (password.isBlank()) {
+            return ValidationResult(
+                successful = false,
+                errorMessage = "Password tidak boleh kosong"
+            )
+        }
+        if (password.length < 8) {
+            return ValidationResult(
+                successful = false,
+                errorMessage = "Password minimal 8 karakter"
+            )
+        }
+        if (!password.contains(kapitalRegex)) {
+            return ValidationResult(
+                successful = false,
+                errorMessage = "Password harus memiliki setidaknya satu huruf besar (kapital)"
+            )
+        }
+        if (!password.contains(lowercaseRegex)) {
+            return ValidationResult(
+                successful = false,
+                errorMessage = "Password harus memiliki setidaknya satu huruf kecil"
+            )
+        }
+        if (!password.contains(numberRegex)) {
+            return ValidationResult(
+                successful = false,
+                errorMessage = "Password harus memiliki setidaknya satu angka"
+            )
+        }
+        if (!password.contains(specialCharRegex)) {
+            return ValidationResult(
+                successful = false,
+                errorMessage = "Password harus memiliki setidaknya satu karakter spesial (!@#$...)"
+            )
+        }
+        return ValidationResult(successful = true)
     }
 }
