0a62874136da9980a6dfcbb2b34e108c2b24d53b
theofanihati <theofanihati@gmail.com>
2025-11-16 12:57:11 +0700

    auth domain email GREEN

--- app/src/main/java/com/dishut_lampung/sitanihut/domain/use_case/auth/ValidateEmailUseCase.kt
+++ app/src/main/java/com/dishut_lampung/sitanihut/domain/use_case/auth/ValidateEmailUseCase.kt
@@ -1,10 +1,33 @@
 package com.dishut_lampung.sitanihut.domain.use_case.auth
 
+import java.util.regex.Pattern
 import com.dishut_lampung.sitanihut.domain.validator.ValidationResult
 
 class ValidateEmailUseCase {
+    companion object {
+        private val EMAIL_ADDRESS_PATTERN: Pattern = Pattern.compile(
+            "[a-zA-Z0-9\\+\\.\\_\\%\\-\\+]{1,256}" +
+                    "\\@" +
+                    "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}" +
+                    "(" +
+                    "\\." +
+                    "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25}" +
+                    ")+"
+        )
+    }
     operator fun invoke(email: String): ValidationResult {
-        // TODO blumm
-        return ValidationResult(false, errorMessage = "belumm")
+        if (email.isBlank()) {
+            return ValidationResult(
+                successful = false,
+                errorMessage = "Email tidak boleh kosong"
+            )
+        }
+        if (!EMAIL_ADDRESS_PATTERN.matcher(email).matches()) {
+            return ValidationResult(
+                successful = false,
+                errorMessage = "Format email tidak valid"
+            )
+        }
+        return ValidationResult(successful = true)
     }
 }
