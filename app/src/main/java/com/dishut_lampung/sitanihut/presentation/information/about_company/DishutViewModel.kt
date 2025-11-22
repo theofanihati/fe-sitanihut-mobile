2689a5620d05d347411fc442b01508b705c47858
theofanihati <theofanihati@gmail.com>
2025-11-22 23:08:42 +0700

    refactor(informasi-presentation): refactor dishut view model, GREEN

--- app/src/main/java/com/dishut_lampung/sitanihut/presentation/information/about_company/DishutViewModel.kt
+++ app/src/main/java/com/dishut_lampung/sitanihut/presentation/information/about_company/DishutViewModel.kt
@@ -1,6 +1,7 @@
 package com.dishut_lampung.sitanihut.presentation.information.about_company
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.dishut_lampung.sitanihut.domain.repository.CompanyRepository
 import com.dishut_lampung.sitanihut.domain.usecase.information.DownloadStructureImageUseCase
 import com.dishut_lampung.sitanihut.presentation.information.InformationEvent
@@ -9,6 +10,7 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -34,7 +36,30 @@ class DishutViewModel @Inject constructor(
     }
 
     private fun downloadStructureImage() {
-        // TODO: lum lah bre
+        _state.update {
+            it.copy(
+                isLoading = true,
+                generalError = null,
+                successMessage = null
+            )
+        }
+
+        viewModelScope.launch {
+            val result = downloadStructureImageUseCase()
+            _state.update { currentState ->
+                if (result.isSuccess) {
+                    currentState.copy(
+                        isLoading = false,
+                        successMessage = result.getOrNull() ?: "Berhasil diunduh"
+                    )
+                } else {
+                    currentState.copy(
+                        isLoading = false,
+                        generalError = result.exceptionOrNull()?.message ?: "Gagal mengunduh gambar"
+                    )
+                }
+            }
+        }
     }
 
 }
