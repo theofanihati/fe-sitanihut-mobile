9f5a5bc06db18284ed13f830be206ab680f7cc66
theofanihati <theofanihati@gmail.com>
2025-11-22 16:34:43 +0700

    test(home-kkph-domain):copy test and code exactly from getPenyuluhHomeDataUseCase (it has similar feat except for preview report). get preview report by status 'pemeriksaan kph', not implemented yet on Back End.

--- app/src/main/java/com/dishut_lampung/sitanihut/domain/usecase/home/GetKkphHomeDataUseCase.kt
+++ app/src/main/java/com/dishut_lampung/sitanihut/domain/usecase/home/GetKkphHomeDataUseCase.kt
@@ -1,4 +1,15 @@
 package com.dishut_lampung.sitanihut.domain.usecase.home
 
-class GetKkphHomeDataUseCase {
+import com.dishut_lampung.sitanihut.domain.model.Report
+import com.dishut_lampung.sitanihut.domain.repository.HomeRepository
+import com.dishut_lampung.sitanihut.util.Resource
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class GetKkphHomeDataUseCase @Inject constructor(
+    private val repository: HomeRepository
+) {
+    operator fun invoke(): Flow<Resource<List<Report>>> {
+        return repository.getReportsByStatus("pemeriksaan kph")
+    }
 }
