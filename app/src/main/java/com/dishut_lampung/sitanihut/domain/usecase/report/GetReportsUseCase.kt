cf6360f1ed6ef2960946a75b048291a3001f410e
theofanihati <theofanihati@gmail.com>
2025-12-02 14:02:25 +0700

    test(report):add domain test for GetReportUseCase RED

---
+++ app/src/main/java/com/dishut_lampung/sitanihut/domain/usecase/report/GetReportsUseCase.kt
@@ -0,0 +1,20 @@
+package com.dishut_lampung.sitanihut.domain.usecase.report
+
+import androidx.paging.PagingData
+import com.dishut_lampung.sitanihut.domain.model.Report
+import com.dishut_lampung.sitanihut.domain.model.ReportStatus
+import com.dishut_lampung.sitanihut.domain.repository.ReportRepository
+import com.dishut_lampung.sitanihut.util.Resource
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class GetReportsUseCase @Inject constructor(
+   private val reportRepository: ReportRepository
+) {
+    operator fun invoke(
+        params: String = "",
+        status: ReportStatus? = null
+    ): Flow<PagingData<Report>> {
+       TODO("belum beb")
+    }
+}
---
+++ app/src/test/java/com/dishut_lampung/sitanihut/domain/usecase/pengajuan_laporan/GetReportsUseCaseTest.kt
@@ -0,0 +1,43 @@
+package com.dishut_lampung.sitanihut.domain.usecase.pengajuan_laporan
+
+import androidx.paging.PagingData
+import com.dishut_lampung.sitanihut.domain.model.Report
+import com.dishut_lampung.sitanihut.domain.model.ReportStatus
+import com.dishut_lampung.sitanihut.domain.repository.ReportRepository
+import com.dishut_lampung.sitanihut.domain.usecase.report.GetReportsUseCase
+import com.dishut_lampung.sitanihut.util.Resource
+import com.google.common.base.Verify.verify
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+
+class GetReportsUseCaseTest {
+    private val repository: ReportRepository = mockk()
+
+    private lateinit var getReportsUseCase: GetReportsUseCase
+
+    @Before
+    fun setUp() {
+        getReportsUseCase = GetReportsUseCase(repository)
+    }
+
+    @Test
+    fun `waktu get dapet laporan dari repo`() = runTest {
+        val dummyPagingData = PagingData.from(emptyList<Report>())
+        every { repository.getReports(any(), any()) } returns flowOf(dummyPagingData)
+
+        val result = getReportsUseCase("", null).first()
+        assertNotNull(result)
+
+        verify(exactly = 1) { repository.getReports("", null) }
+    }
+}
