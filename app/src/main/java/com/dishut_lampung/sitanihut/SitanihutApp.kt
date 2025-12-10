f91579a2537bba1cbef1bb15f5387da02c4b5a96
theofanihati <theofanihati@gmail.com>
2025-12-10 11:49:07 +0700

    chore:add worker to manifest

--- app/src/main/java/com/dishut_lampung/sitanihut/SitanihutApp.kt
+++ app/src/main/java/com/dishut_lampung/sitanihut/SitanihutApp.kt
@@ -1,7 +1,18 @@
 package com.dishut_lampung.sitanihut
 
 import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
-class SitanihutApp : Application()
+class SitanihutApp : Application(), Configuration.Provider {
+    @Inject
+    lateinit var workerFactory: HiltWorkerFactory
+
+    override val workManagerConfiguration: Configuration
+    get() = Configuration.Builder()
+        .setWorkerFactory(workerFactory)
+        .build()
+}
