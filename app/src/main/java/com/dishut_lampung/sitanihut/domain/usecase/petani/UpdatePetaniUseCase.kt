79cfbe65d3d1b02f0f50e2f18a7c41283096acc3
theofanihati <theofanihati@gmail.com>
2026-01-12 10:10:56 +0700

    test(petani-domain):add test for UpdatePetaniUseCase RED

---
+++ app/src/main/java/com/dishut_lampung/sitanihut/domain/usecase/petani/UpdatePetaniUseCase.kt
@@ -0,0 +1,14 @@
+package com.dishut_lampung.sitanihut.domain.usecase.petani
+
+import com.dishut_lampung.sitanihut.domain.model.CreatePetaniInput
+import com.dishut_lampung.sitanihut.domain.repository.PetaniRepository
+import com.dishut_lampung.sitanihut.util.Resource
+import javax.inject.Inject
+
+class UpdatePetaniUseCase @Inject constructor(
+    private val repository: PetaniRepository
+) {
+    suspend operator fun invoke(id: String, input: CreatePetaniInput): Resource<Unit> {
+        return TODO()
+    }
+}
---
+++ app/src/test/java/com/dishut_lampung/sitanihut/domain/usecase/petani/UpdatePetaniUseCaseTest.kt
@@ -0,0 +1,52 @@
+package com.dishut_lampung.sitanihut.domain.usecase.petani
+
+import com.dishut_lampung.sitanihut.domain.model.CreatePetaniInput
+import com.dishut_lampung.sitanihut.domain.repository.PetaniRepository
+import com.dishut_lampung.sitanihut.util.Resource
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.mockk
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+
+class UpdatePetaniUseCaseTest {
+
+    private lateinit var repository: PetaniRepository
+    private lateinit var useCase: UpdatePetaniUseCase
+
+    @Before
+    fun setUp() {
+        repository = mockk()
+        useCase = UpdatePetaniUseCase(repository)
+    }
+
+    @Test
+    fun `invoke should return Success when repository update is successful`() = runTest {
+        val id = "123"
+        val input = mockk<CreatePetaniInput>(relaxed = true)
+        val expectedResult = Resource.Success(Unit)
+
+        coEvery { repository.updatePetani(id, input) } returns expectedResult
+
+        val result = useCase(id, input)
+        assertEquals(expectedResult, result)
+
+        coVerify(exactly = 1) { repository.updatePetani(id, input) }
+    }
+
+    @Test
+    fun `invoke should return Error when repository update fails`() = runTest {
+        val id = "123"
+        val input = mockk<CreatePetaniInput>(relaxed = true)
+        val errorMessage = "Gagal update data"
+        val expectedResult = Resource.Error<Unit>(errorMessage)
+
+        coEvery { repository.updatePetani(id, input) } returns expectedResult
+
+        val result = useCase(id, input)
+        assertEquals(expectedResult, result)
+        coVerify(exactly = 1) { repository.updatePetani(id, input) }
+    }
+}
